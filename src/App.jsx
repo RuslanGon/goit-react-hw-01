a0fbf83b9a22499148896cb12ca5c51ef4f97860
RuslanGon <ruslan441006@gmail.com>
2024-03-06 17:24:48 +0200

    run

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import MailBox from "./components/MailBox";
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Vite + React</h1>
-      <p>Click on the Vite and React logos to learn more</p>
+    <div>
       <MailBox boxTitle="Meest Expres" mailBoxCount={5} />
       <MailBox boxTitle="Nova Poshta" mailBoxCount={3} />
       <MailBox boxTitle="UKR poshta" />
--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -2,8 +2,7 @@ const MailBox = ({ boxTitle, mailBoxCount = 0 }) => {
   return (
     <div>
       <h2>{boxTitle}</h2>
-      <p>количество активных ящиков: {mailBoxCount}</p>
-      <p><b>сейчас нет активных ящиков</b></p>
+      {mailBoxCount === 0 ? <p><b>приносим извенения, сейчас нет активных ящиков</b></p> : <p>количество активных ящиков: {mailBoxCount}</p>}
       <ul>
         <li>max@example.com</li>
         <li>merry@example.com</li>
