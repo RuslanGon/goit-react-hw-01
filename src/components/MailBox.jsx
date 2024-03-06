de3a6e16daf9472cac45ae51567f4584ceafd1d6
RuslanGon <ruslan441006@gmail.com>
2024-03-06 18:20:27 +0200

    rete

--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -4,7 +4,7 @@ const MailBox = ({ boxUsers, boxTitle, mailBoxCount = 0 }) => {
       <h2>{boxTitle}</h2>
       {mailBoxCount === 0 ? <p><b>приносим извенения, сейчас нет активных ящиков</b></p> : <p>количество активных ящиков: {mailBoxCount}</p>}
       <ul>
-       {boxUsers.map(user => {
+       {boxUsers?.map(user => {
         return <li key={user.id}>Email: {user.usermail}</li>
        })}
       </ul>
