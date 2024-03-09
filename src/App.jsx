eb17b7bdd67c4d342d9a9c2e9d79015402afe231
RuslanGon <ruslan441006@gmail.com>
2024-03-09 09:39:45 +0200

    dcd

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
-// import userData from '../userData.json';
-// import friends from '../friends.json';
-// import transactions from '../transactions.json';
-// import Profile from './Profile/Profile';
-// import FriendList from './FriendList/FriendList';
-// import TransactionHistory from './TransactionHistory/TransactionHistory';
- 
+// import { useState } from 'react'
 
+import userData from '../userData.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import Profile from './Profile/Profile';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import '../App.css';
 
-
-import MailBox from "./components/MailBox";
-
-const meesExpress = [
-  {id: "1", usermail: "misha@example.com"},
-  {id: "2", usermail: "merry@example.com"},
-  {id: "3", usermail: "alex@example.com"}
-];
-
-const novaPoshta = [
-  {id: "11", usermail: "igor@example.com"},
-  {id: "22", usermail: "roma@example.com"},
-  {id: "33", usermail: "lena@example.com"}
-];
-
-const ukrPoshta = [
-  {id: "111", usermail: "marina@example.com"},
-  {id: "222", usermail: "ola@example.com"},
-  {id: "333", usermail: "sereja@example.com"},
-  {id: "444", usermail: "oksana@example.com"}
-];
-
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <MailBox boxUsers={meesExpress} boxTitle="Meest Expres" mailBoxCount={5} />
-      <MailBox boxUsers={novaPoshta} boxTitle="Nova Poshta" mailBoxCount={3} />
-      <MailBox boxUsers={ukrPoshta} boxTitle="UKR poshta" />
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
-}
+};
 
 export default App;
