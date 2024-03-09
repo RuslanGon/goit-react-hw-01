a98b7e937adeed906c9b8b93a4c3ac74d1288d19
RuslanGon <ruslan441006@gmail.com>
2024-03-09 09:30:30 +0200

    dcd

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,30 @@
+import css from './Profile.module.css';
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
+    <div className={css.profileCard}>
+      {' '}
       <div>
-        <div>
-          <img
-            src={image}
-            alt="User avatar"
-          />
-          <p>{name}</p>
-          <p>@{tag}</p>
-          <p>{location}</p>
-        </div>
-
-        <ul>
-          <li>
-            <span>Followers</span>
-            <span>{stats.followers}</span>
-          </li>
-          <li>
-            <span>Views</span>
-            <span>{stats.views}</span>
-          </li>
-          <li>
-            <span>Likes</span>
-            <span>{stats.likes}</span>
-          </li>
-        </ul>
-      </div>
+        <img className={css.profileImage} src={image} alt="User avatar" />
+        <p className={css.name}>{name}</p>{' '}
+        <p className={css.personalInfo}>@{tag}</p>{' '}
+        <p className={css.personalInfo}>{location}</p>{' '}
+      </div>{' '}
+      <ul className={css.statsList}>
+        {' '}
+        <li className={css.statsItem}>
+          <span className={css.statsName}>Followers</span>{' '}
+          <span className={css.statsInfo}>{stats.followers}</span>{' '}
+        </li>{' '}
+        <li className={css.statsItem}>
+          <span className={css.statsName}>Views</span>{' '}
+          <span className={css.statsInfo}>{stats.views}</span>{' '}
+        </li>{' '}
+        <li className={css.statsItem}>
+          <span className={css.statsName}>Likes</span>{' '}
+          <span className={css.statsInfo}>{stats.likes}</span>{' '}
+        </li>{' '}
+      </ul>
     </div>
   );
 };
-
 export default Profile;
